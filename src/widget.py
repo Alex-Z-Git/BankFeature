2500482306f47f58df72551bb42a8b2f77c6fc48
Александр Зонов <ooo.elliss@yandex.ru>
2026-01-15 17:11:33 +0300

    add to widget function get_date using re

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+import re
+
 from masks import get_mask_account
 from masks import  get_mask_card_number
 
@@ -26,8 +28,15 @@ def mask_account_card(card_data: str) -> str:
         return text + mask_card_num
 
 
+def get_date(some_date: str) -> str:
+    match_date = re.search(r"(\d{4})-(\d{2})-(\d{2})", some_date)
+    # formatted_date = re.sub(r"(\d{4})-(\d{2})-(\d{2})", r"\3.\2.\1", some_date)
+    return f"{match_date.group(3)}.{match_date.group(2)}.{match_date.group(1)}"
+#    print(formatted_date)  # 11.03.2024
+
 
 
 if __name__ == "__main__":
     print(mask_account_card("Счет 1234567898765432"))
     print(mask_account_card("Visa Master 4567898765432"))
+    print(get_date("2024-03-11T02:26:18.671407"))
