a14871013423bc35eaf83f6727e217dca811de57
Alexander Zonov <ooo.elliss@yandex.ru>
2026-02-09 15:50:52 +0300

    Make tests for module masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,21 @@
 from typing import Union
 
 
+
+
 def get_mask_card_number(number: Union[int, str]) -> str:
     """Функция, которая принимает номер карты в виде числа или строки
      и возвращает замаскированный номер карты вида '7000 79** **** 6361' """
 
     temp_number = str(number)
-    mask_number = temp_number[:4] + " " + temp_number[4:6] + "**" + " **** " + temp_number[-4:]
+    formated_temp_number = temp_number.replace(" ", "")
+
+    if formated_temp_number.isdigit() and len(formated_temp_number) == 16:
+
+        mask_number = formated_temp_number[:4] + " " + formated_temp_number[4:6] + "**" + " **** " + formated_temp_number[-4:]
+
+    else:
+        raise ValueError("Неверный формат данных")
 
     return mask_number
 
@@ -16,11 +25,19 @@ def get_mask_account(bill: Union[int, str]) -> str:
     и возвращает замаскированный номер счета вида '**6361' """
 
     temp_bill = str(bill)
-    mask_bill = "**" + temp_bill[-4:]
+    formated_temp_bill = temp_bill.replace(" ", "")
+
+    if formated_temp_bill.isdigit() and len(formated_temp_bill) == 20:
+        mask_bill = "**" + temp_bill[-4:]
+    else:
+        raise ValueError("Неверный формат данных")
 
     return mask_bill
 
 
 if __name__ == "__main__":
     print(get_mask_card_number(1234567898765432))
+    print(get_mask_card_number("1234 5678 9876 5432"))
+
     print(get_mask_account(73654108430135874305))
+    print(get_mask_account("12345678998765432100"))
---
+++ tests/test_masks.py
@@ -0,0 +1,41 @@
+import pytest
+from src.masks import get_mask_account, get_mask_card_number
+
+@pytest.mark.parametrize ("card_number, expected", [(1234567898765432, "1234 56** **** 5432"),
+                                                    ("9876543223456789", "9876 54** **** 6789"),
+                                                    ("1234 5678 9876 0123", "1234 56** **** 0123")
+                                                    ])
+def test_get_mask_card_number_basic(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
+
+@pytest.mark.parametrize ("card_number", [(1234567765),
+                                          (4345657687767464345434557656),
+                                          ("9876 54** **** 6789"),
+                                          ("asdfghjkllkjhgfd"),
+                                          ([9876, 5432, 1098, 7654]),
+                                          ("1234:5678:8765:4321")
+                                          ])
+def test_get_mask_card_number_ValueErrors(card_number):
+    with pytest.raises(ValueError):
+        get_mask_card_number(card_number)
+
+
+@pytest.mark.parametrize ("bill_number, expected", [(98765432112345675432, "**5432"),
+                                                    ("123456789998765436789", "**6789"),
+                                                    ("1234 5678 5555 9876 0123", "**0123")
+                                                    ])
+def test_get_mask_account_basic(bill_number, expected):
+    assert get_mask_account(bill_number) == expected
+
+
+@pytest.mark.parametrize ("bill_number", [(1234567765),
+                                          (4345657687767464345434557656),
+                                          ("9876 54** **** 6789"),
+                                          ("asdfghjkllkjhgfddjhf"),
+                                          ([9876, 5432, 1098, 7654]),
+                                          ("1234:5678:8765:4321:5647")
+                                          ])
+def test_get_mask_account_ValueErrors(bill_number):
+    with pytest.raises(ValueError):
+        get_mask_card_number(bill_number)
